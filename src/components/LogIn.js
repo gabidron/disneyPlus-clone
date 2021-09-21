927043c2a6612b16650ccbd1ff71a1fbf137cc7f
gabidron <drongabi8@gmail.com>
2021-09-21 23:55:57 +0300

    Login page ui

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,22 +1,80 @@
 import React from "react";
 import styled from "styled-components";
 const LogIn = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <CTA>
+        <CTALogoOne src="/images/cta-logo-one.svg" />
+        <SignUp>Get All there</SignUp>
+        <Description>
+          Get Premier Access to Raya and the Last Dragon for an aditional fee
+          with a Disney+ subscription. As of 03/26/21, the price of Disney+ and
+          the Disney Bundle will increse by $1.
+        </Description>
+        <CTALogoTwo src="/images/cta-logo-two.png" />
+      </CTA>
+    </Container>
+  );
 };
 
 export default LogIn;
 const Container = styled.div`
   position: relative;
-  height: calc(100vh-60px);
+  display: flex;
+  align-items: top;
+  justify-content: center;
+  min-height: calc(100vh - 70px);
   &:before {
-    background: url("/images/home-background.png") center center / cover
-      no-repeat fixed;
-    content: "df";
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-image: url("/images/login-background.jpg");
+    content: "";
     position: absolute;
     top: 0;
     left: 0;
     right: 0;
     bottom: 0;
+    opacity: 0.7;
     z-index: -1;
   }
+`;
+const CTA = styled.div`
+  width: 80%;
+  max-width: 615px;
+  padding: 80px 40px;
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+  align-items: center;
+`;
+const CTALogoOne = styled.img``;
+const CTALogoTwo = styled.img`
+  width: 98%;
+`;
+const SignUp = styled.a`
+  width: 100%;
+  background-color: #0063e5;
+  font-weight: bold;
+  text-transform: uppercase;
+  padding: 17px 0;
+  color: #f9f9f9;
+  border-radius: 4px;
+  text-align: center;
+  font-size: 18px;
+  cursor: pointer;
+  transition: all 250ms;
+  letter-spacing: 1.5px;
+  margin-top: 8px;
+  margin-bottom: 12px;
+
+  &:hover {
+    background-color: #0483ee;
+  }
+`;
+const Description = styled.p`
+  font-size: 11px;
+  letter-spacing: 1.5px;
+  text-align: center;
+  line-height: 1.5;
 `;
